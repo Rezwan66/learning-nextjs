2eb96d0f2298ec06cd311d2817e5e5a7300b2df4
Rezwan66 <shaikhrezwan66@gmail.com>
2024-02-05 01:24:11 +0100

    routing and navigation for internal routing done using Link tag from next-app

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,8 +1,19 @@
+import Link from 'next/link';
+
 const AboutLayout = ({ children }) => {
   return (
     <>
       <div>
-        <nav className="my-6">Mission | Vision</nav>
+        <nav className="my-6">
+          <ul className="flex gap-4">
+            <li>
+              <Link href="/about/mission">Mission</Link>
+            </li>
+            <li>
+              <Link href="/about/vision">Vision</Link>
+            </li>
+          </ul>
+        </nav>
         {children}
       </div>
     </>
