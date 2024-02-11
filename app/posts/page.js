e80d28594c5db29fb7902a6d97756f3dfa1c0aba
Rezwan66 <shaikhrezwan66@gmail.com>
2024-02-11 22:34:20 +0100

    displayed all the posts titles on the posts route

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,11 +1,22 @@
 import getAllPosts from '@/lib/getAllPosts';
+import Link from 'next/link';
 
 export default async function Posts() {
   const posts = await getAllPosts();
-  console.log(posts);
+  //   console.log(posts);
   return (
     <div className="mt-6">
       <h1> All Posts: {posts.length} </h1>
+      <ul>
+        {posts.map(post => (
+          <li
+            className="border border-blue-400 max-w-[800px] hover:bg-yellow-200"
+            key={post.id}
+          >
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
